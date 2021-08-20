2e667795a47b1b8b6b9201a8eda89b7c41a24655
rbontekoe <rbontekoe@appligate.nl>
2021-08-20 10:42:52 +0200

    Test with external source for invoice_nbrs

--- src/infrastructure/Infrastructure.jl
+++ src/infrastructure/Infrastructure.jl
@@ -4,6 +4,7 @@ module Infrastructure
 
 include("./db.jl")
 include("./doc.jl") # database functions
+include("./counter.jl") # sequence number invoice
 
 using ..AppliAR
 
@@ -44,22 +45,23 @@ end # read_bank_statements
 
 process(orders::Array{Order, 1}; path=FILE_UNPAID_INVOICES) = begin
     # get last invoice number
-    try
-        read_from_file(FILE_INVOICE_NBR)
-    catch e
-        add_to_file(FILE_INVOICE_NBR, [START_INVOICE_NBR])
-    end
+    #try
+    #    read_from_file(FILE_INVOICE_NBR)
+    #catch e
+    #    add_to_file(FILE_INVOICE_NBR, [START_INVOICE_NBR])
+    #end
 
-    invnbr = last(read_from_file(FILE_INVOICE_NBR))
+    #invnbr = last(read_from_file(FILE_INVOICE_NBR))
 
     # run in kubernetes
     key = haskey(ENV, "HOSTNAME") ? split(ENV["HOSTNAME"], "-")[1] * split(ENV["HOSTNAME"], "-")[3] : "A"
 
     # create invoices
-    invoices = [create(order, key * "-" * string(invnbr += 1)) for order in orders]
-
+    #invoices = [create(order, key * "-" * string(invnbr += 1)) for order in orders]
+    invoices = [create(order, key * "-" * string(retrieve_invoice_nbr("ABC", ip"192.168.2.40", 2020)) for order in orders]
+    
     # save invoice number
-    add_to_file(FILE_INVOICE_NBR, [invnbr])
+    #add_to_file(FILE_INVOICE_NBR, [invnbr])
 
     # archive invoices
     add_to_file(path, invoices)
