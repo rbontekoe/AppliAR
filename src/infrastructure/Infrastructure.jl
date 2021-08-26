8bd2a62e2b60a8bd263de7fb2a533574a1397cc0
rbontekoe <rbontekoe@appligate.nl>
2021-08-26 16:39:18 +0200

    Back to initial state

--- src/infrastructure/Infrastructure.jl
+++ src/infrastructure/Infrastructure.jl
@@ -58,9 +58,9 @@ process(orders::Array{Order, 1}; path=FILE_UNPAID_INVOICES) = begin
 
     # create invoices
     #invoices = [create(order, key * "-" * string(invnbr += 1)) for order in orders]
-    #invoices = [create(order, key * "-" * string(retrieve_invoice_nbr("ABC", ip"192.168.2.40", 30014))) for order in orders]
+    invoices = [create(order, key * "-" * string(retrieve_invoice_nbr("ABC", ip"192.168.2.40", 30014))) for order in orders]
     
-    invoices = [create(order, key * "-" * string(retrieve_invoice_nbr(ENV["CNTNAME"], IPv4(ENV["CNTIP"]), parse(Int64, ENV["CNTPORT"])))) for order in orders]
+    #invoices = [create(order, key * "-" * string(retrieve_invoice_nbr(ENV["CNTNAME"], IPv4(ENV["CNTIP"]), parse(Int64, ENV["CNTPORT"])))) for order in orders]
     
     # save invoice number
     #add_to_file(FILE_INVOICE_NBR, [invnbr])
